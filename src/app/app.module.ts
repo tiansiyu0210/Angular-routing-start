562fb056503a2b0b707c877f2cdcc6536c7f4c5c
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-30 17:35:53 -0400

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'servers', component: ServersComponent},
+  {path: 'users', component: UsersComponent}
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +32,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
