4d7719ff79042d0271e956e7cbb0b9bba68ac233
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-31 14:40:18 -0400

    outsourcing the route module

---
+++ src/app/app-route.module.ts
@@ -0,0 +1,33 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ServersComponent} from './servers/servers.component';
+import {ServerComponent} from './servers/server/server.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {UsersComponent} from './users/users.component';
+import {UserComponent} from './users/user/user.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'servers', component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent}
+    ]},
+  {path: 'users', component: UsersComponent, children:[
+      {path: ':id/:name', component: UserComponent}
+    ]},
+  {path: 'something', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/something', pathMatch: 'full'},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRouteModule{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes} from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRouteModule} from './app-route.module';
 
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'servers', component: ServersComponent, children: [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent}
-    ]},
-  {path: 'users', component: UsersComponent, children:[
-      {path: ':id/:name', component: UserComponent}
-    ]},
-  {path: 'something', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/something'},
-
-];
 
 @NgModule({
   declarations: [
@@ -43,7 +30,7 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes)
+    AppRouteModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
